cf7f5229e1da4158a632dce2a9ed4872d6c7a3b3
AminaZafar-1998 <68804288+AminaZafar-1998@users.noreply.github.com>
2021-03-31 16:38:09 +0500

    commits

---
+++ pages/signup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <nav-bar></nav-bar>
+<diet-signup></diet-signup>
+  <footer-section></footer-section>
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/NavBar";
+import DietSignup from "../components/DietSignup";
+import FooterSection from "../components/FooterSection";
+export default {
+  name: "signup",
+  components: {NavBar,
+    DietSignup,
+    FooterSection
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
