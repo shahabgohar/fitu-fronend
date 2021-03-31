bec1b50e8be84f119748e4f32eb86a6174529ca9
AminaZafar-1998 <68804288+AminaZafar-1998@users.noreply.github.com>
2021-03-31 16:37:27 +0500

    commits

---
+++ pages/Signin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <nav-bar></nav-bar>
+<diet-sign></diet-sign>
+    <footer-section></footer-section>
+  </div>
+</template>
+
+<script>
+import DietSign from "../components/DietSign";
+import NavBar from "../components/NavBar";
+import FooterSection from "../components/FooterSection";
+export default {
+name: "Signin",
+  components:
+{
+  FooterSection,
+  NavBar,
+  DietSign
+}
+
+}
+</script>
+
+<style scoped>
+
+</style>
